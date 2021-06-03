b7e0c2b9a165d14018d9bb2e381bae7ec06cc29d
root <arthur.berthelier@gmail.com>
2021-06-03 12:21:28 +0000

    add simple tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
+
+
 # Create your tests here.
+
+
+
+
+from django.test import SimpleTestCase
+
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/') # notre homepage
+        self.assertEqual(response.status_code, 200) #200 = OK
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+
+
+
+
